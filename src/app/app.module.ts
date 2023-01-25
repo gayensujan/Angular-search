07effef23d3dbdb2f16e9d876cf97bd174c56894
Sujan Gayen <sujan.gayen@peopletech.com>
2023-01-25 14:36:18 +0530

    search commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    HomeComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    ReactiveFormsModule
+ 
+  
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
